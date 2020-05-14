75685e8286fb7347a2a47f2a35253450de9c16f3
st3fan7 <58364787+st3fan7@users.noreply.github.com>
2020-05-14 19:27:15 +0200

    Implementacija izgleda aplikacije za lekara - WPF

---
+++ HCI/Lekar - Stefan Arađanin/HealthClinic/HealthClinic/Login.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HealthClinic
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (txtUserName.Text.Equals("pera") && txtPassword.Password.ToString().Equals("pera"))
+            {
+                
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Username or password is not valid");
+            }
+        }
+
+        
+    }
+}
---
+++ HCI/Lekar - Stefan Arađanin/HealthClinic/HealthClinic/UserControlPregled.xaml.cs
@@ -0,0 +1,50 @@
+﻿                                                                                                                                         
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HealthClinic.Entiteti;
+
+namespace HealthClinic
+{
+    /// <summary>
+    /// Interaction logic for UserControlPregled.xaml
+    /// </summary>
+    public partial class UserControlPregled : UserControl
+    {
+        public ObservableCollection<Pacijent> Pacijenti
+        {
+            get;
+            set;
+        }
+
+        public UserControlPregled()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Pacijenti = UserControlPocetna.Pacijenti;
+        }
+
+        private void ListViewPregledPacijenti_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GridPregled.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonDijanoza_Click(object sender, RoutedEventArgs e)
+        {
+            DijalogIzmenaDijagnoze dijalogDijagnoza = new DijalogIzmenaDijagnoze();
+            dijalogDijagnoza.ShowDialog();
+        }
+    }
+}
