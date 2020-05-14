5c600659072b0a02d0bdeae534f94787ef31495d
aleksaivanic <ivanicaleksa98@gmail.com>
2020-05-14 20:49:57 +0200

    Implementacija izgleda aplikacije za pacijenta - WPF

---
+++ HCI/Pacijent - Aleksa Ivanić/HealthClinic/HealthClinic/Model/Appointments.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthClinic.Model
+{
+    public class Appointments
+    {
+        private string termDate;
+        private string termTime;
+        private string doctor;
+
+        public string Datum
+        {
+            get
+            {
+                return termDate;
+            }
+            set
+            {
+                if (value != termDate)
+                {
+                    termDate = value;
+                }
+            }
+        }
+
+        public string Termin
+        {
+            get
+            {
+                return termTime;
+            }
+            set
+            {
+                if (value != termTime)
+                {
+                    termTime = value;
+                }
+            }
+        }
+
+        public string Lekar
+        {
+            get
+            {
+                return doctor;
+            }
+            set
+            {
+                if (value != doctor)
+                {
+                    doctor = value;
+                }
+            }
+        }
+    }
+}
